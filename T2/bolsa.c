30892abe6b680b42cb87dac6719b30e3463d05f9
Samu.py <80702161+Samu-2@users.noreply.github.com>
2022-09-24 17:59:04 -0300

    -

--- T2/bolsa.c
+++ T2/bolsa.c
@@ -2,68 +2,81 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <string.h>
-
 #include "bolsa.h"
 
-pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t  n = PTHREAD_COND_INITIALIZER;
-int*   p = NULL;
-int*   e;
-char** s;
-char** b;
+//Global Variables
+//Logic
+int *isSold; //if is 1, so was sold
+
+//Info of seller
+char **nameSeller;
+int *priceSeller = NULL; //Cond initial is Null
+
+//Info of buyer
+char **nameBuyer;
+
+//Cond and Mutex of pthread
+static pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t c = PTHREAD_COND_INITIALIZER;
 
 int vendo(int precio, char *vendedor, char *comprador) {
-  int r = 0;
-  pthread_mutex_lock(&m);
+    pthread_mutex_lock(&m);
+    int* myPrice = malloc(sizeof(int));
+    *myPrice = precio;//init: our price
+    int* myIsSold = malloc(sizeof(int));
+    *myIsSold = 0; //init: false
+    char** myBuyer = malloc(sizeof(char*));
+    *myBuyer = comprador;//init: this will modify our buyer
+    char** myseller = malloc(sizeof(char*));
+    *myseller = vendedor;//init: our name
 
-  int* mp   = malloc(sizeof(int));
-  *mp = precio;
-  int* me   = malloc(sizeof(int));
-  *me = 0;
-  char** ms = malloc(sizeof(char*));
-  *ms = vendedor;
-  char** mb = malloc(sizeof(char*));
-  *mb = comprador;
+    if(priceSeller == NULL){//if not exist a seller, so we change the global data with the own data
+        isSold = myIsSold;
+        priceSeller = myPrice;
+        nameSeller = myseller;
+        nameBuyer = myBuyer;
+    }
+    else if(*priceSeller > *myPrice){//if we have better price than the actually seller, so we change the global data with the own data
+        isSold = myIsSold;
+        priceSeller = myPrice;
+        nameSeller = myseller;
+        nameBuyer = myBuyer;
+        pthread_cond_broadcast(&c);// and we wake up the previus seller
+    }
 
-  if (p == NULL) {
-    p = mp;
-    e = me;
-    s = ms;
-    b = mb;
-  }
-  else if (*p > *mp) {
-    p = mp;
-    e = me;
-    s = ms;
-    b = mb;
-    pthread_cond_broadcast(&n);
-  }
-  pthread_cond_wait(&n, &m);
-  if (*me == 1) {
-    r = 1;
-  }
-  free(mp);
-  free(me);
-  free(ms);
-  free(mb);
-  pthread_mutex_unlock(&m);
-  return r;
+    pthread_cond_wait(&c,&m);
+    int t = 0; //comparator
+    if(*myIsSold == 1){
+        t = 1; //now we return 1 
+    }
+    //we free the memory
+    free(myPrice);
+    free(myBuyer);
+    free(myIsSold);
+    free(myseller);
+    pthread_mutex_unlock(&m); //Our information is the pointer 
+    return t;
 }
 
+
 int compro(char *comprador, char *vendedor) {
-  int r = 0;
-  pthread_mutex_lock(&m);
-  if (p != NULL) {
-    strcpy(*b, comprador);
-    strcpy(vendedor, *s);
-    *e = 1;
-    r = *p;
-    p = NULL;
-    e = NULL;
-    s = NULL;
-    b = NULL;
-    pthread_cond_broadcast(&n);
-  }
-  pthread_mutex_unlock(&m);
-  return r;
+    pthread_mutex_lock(&m);
+    int r = 0;
+    if (priceSeller != NULL) {
+      //we obtain the seller´s data
+      strcpy(vendedor,*nameSeller);
+      strcpy(*nameBuyer,comprador);//now the seller has the seller's name
+      r = *priceSeller;
+      *isSold = 1;
+      //and change the buyer's data with the own
+
+      //we erase the pointer (the seller has this info)
+      isSold = NULL;
+      nameBuyer = NULL;
+      nameSeller = NULL;
+      priceSeller = NULL;
+      pthread_cond_broadcast(&c);
+    }
+    pthread_mutex_unlock(&m);
+    return r;
 }
---
+++ T2/prueba.c
@@ -0,0 +1,79 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+
+#include "bolsa.h"
+
+// Informacion logica del problema
+int exisVendedor = 0;
+int isVendido = 0;
+
+// Informacion vendedor 
+char vendedorGlob;
+int precioGlob;
+
+// informacion comprador 
+char compradorGlob;
+
+// Pthread mutex and cond
+static pthread_cond_t c = PTHREAD_COND_INITIALIZER;
+static pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+
+int vendo(int precio, char vendedor, char comprador) {
+    // if not exist seller, so push the new seller in the market
+    while (isVendido == 1){
+        pthread_cond_wait(&c,&m);
+    }
+    printf("inicio vendedor %s y con una oferta de %i \n",vendedor,precio);
+    pthread_mutex_lock(&m);
+    if ((exisVendedor == 0)  (precioGlob  precio)){
+        we subtitute the var global for information of new seller
+        vendedorGlob = vendedor;
+        precioGlob = precio;
+        exisVendedor = 1; now exist seller, so exisVendor is 1
+        printf(precio remplazado, precio actual es %i, lo vende %sn, precioGlob,vendedor);
+        pthread_cond_broadcast(&c);
+    }
+    else{
+        pthread_mutex_unlock(&m);
+        return 0;
+    }
+    pthread_cond_wait(&c,&m);
+    printf(desperto el vendedor %sn,vendedor);
+
+    if(isVendido == 1){
+        strcpy(comprador,compradorGlob);
+        printf(fue comprado por %in,precio);
+        precioGlob = 0;
+        isVendido = 0;
+        exisVendedor = 0;
+        pthread_mutex_unlock(&m);
+        return 1;
+    }
+    pthread_mutex_unlock(&m);
+    return 0;
+
+}
+
+int compro(char comprador, char vendedor) {
+    pthread_mutex_lock(&m);
+    while (isVendido == 1){
+        pthread_cond_wait(&c,&m);
+    }
+    printf(inicio comprador %s n,comprador);
+    if (exisVendedor == 1){
+        strcpy(vendedor,vendedorGlob);
+        compradorGlob = comprador;
+        int pricePay = precioGlob;
+        isVendido = 1;
+        printf(precio pagado por %s %in,comprador , pricePay);
+        pthread_cond_broadcast(&c);
+        pthread_mutex_unlock(&m);
+        return pricePay;
+    }
+    printf(no hay nada a la venta cuando el comprado %s, paso n,comprador);
+    pthread_mutex_unlock(&m);
+    return 0;
+
+}
